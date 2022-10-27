f1a6c45bb966872543d3be3d3c461815ef12f97a
Day Trevisan <110201520+daytrevdev@users.noreply.github.com>
2022-10-27 13:41:57 -0300

    start-java-projects

---
+++ ContandoAteDez.java
@@ -0,0 +1,10 @@
+public class ContandoAteDez {
+    public static void main(String[] args) {
+
+        System.out.println("Contando ate dez...");
+
+       for(int i=0; i<=10; i++) {
+           System.out.print(i + " ");
+       }
+    }
+}
